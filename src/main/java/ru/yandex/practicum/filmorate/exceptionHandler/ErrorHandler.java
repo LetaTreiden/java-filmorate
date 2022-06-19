4a1d1eccb12e5f7fafb18c132af132cb57309d8f
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2022-06-19 18:15:06 +0300

    9.4

--- src/main/java/ru/yandex/practicum/filmorate/exceptionHandler/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptionHandler/ErrorHandler.java
@@ -4,36 +4,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 
 @RestControllerAdvice("ru.yandex.practicum.filmorate")
 public class ErrorHandler {
 
-    /**
-     * Ошибка валидации, статус код 400.
-     */
+    //400, Ошибка валидации
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleValidationError(final ValidationException e) {
-        return new ErrorResponse("error", e.getMessage());
+    public Error handleValidationError(final ValidationException e) {
+        return new Error("Ошибка валидации", e.getMessage());
     }
 
-    /**
-     * Ошибка поиска, статус код 404.
-     */
+    //404, Ошибка поиска
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleSearchError(final Exception e) {
-        return new ErrorResponse("error", e.getMessage());
+    public Error handleSearchError(final NotFoundException e) {
+        return new Error("Ошибка поиска", e.getMessage());
     }
 
-    /**
-     * Исключение, статус код 500.
-     */
+    //500, Исключение
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleException(final Throwable e) {
-        return new ErrorResponse("error", e.getMessage());
+    public Error handleException(final Throwable e) {
+        return new Error("Исключение", e.getMessage());
     }
 
 }
